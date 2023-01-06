754af789d672e738e172a1780875240413f9e832
nepsoul <nirumagar901@gmail.com>
2023-01-06 15:44:41 +0545

    chore: refactoring the components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React from "react";
-import { toggleImportanceOf, createNote } from "./reducers/noteReducer";
-import { useSelector, useDispatch } from "react-redux";
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    //console.log(content, "content");
-    event.target.note.value = "";
-    // store.dispatch({
-    //   type: "NEW_NOTE",
-    //   data: {
-    //     content,
-    //     important: false,
-    //     id: generateId(),
-    //   },
-    // });
-
-    dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id));
-  };
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   );
 };
