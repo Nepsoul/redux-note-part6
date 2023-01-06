633f53eefbecef049723486fc66def9769f33ba9
nepsoul <nirumagar901@gmail.com>
2023-01-06 15:03:07 +0545

    chore: forwarding Redux store to various components using API hooks of react-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React from "react";
+import { toggleImportanceOf, createNote } from "./reducers/noteReducer";
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    //console.log(content, "content");
+    event.target.note.value = "";
+    // store.dispatch({
+    //   type: "NEW_NOTE",
+    //   data: {
+    //     content,
+    //     important: false,
+    //     id: generateId(),
+    //   },
+    // });
+
+    dispatch(createNote(content));
+  };
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id));
+  };
+
   return (
     <div>
-     
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,119 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
 
-const noteReducer = (state = [], action) => {
-  //   if (action.type === "NEW_NOTE") {
-  //     //state.push(action.data);
-  //     //return state;
-  //     return [...state, action.data];
-  //   }
+import noteReducer from "./reducers/noteReducer";
 
-  //   return state;
+// const noteReducer = (state = [], action) => {
+//   //   if (action.type === "NEW_NOTE") {
+//   //     //state.push(action.data);
+//   //     //return state;
+//   //     return [...state, action.data];
+//   //   }
 
-  switch (action.type) {
-    case "NEW_NOTE":
-      return state.concat(action.data);
-    case "TOGGLE_IMPORTANCE": {
-      const id = action.data.id;
-      const noteToChange = state.find((n) => n.id === id);
-      const changedNote = {
-        ...noteToChange,
-        important: !noteToChange.important,
-      };
-      return state.map((note) => (note.id !== id ? note : changedNote));
-    }
-    default:
-      return state;
-  }
-};
+//   //   return state;
+
+//   switch (action.type) {
+//     case "NEW_NOTE":
+//       return state.concat(action.data);
+//     case "TOGGLE_IMPORTANCE": {
+//       const id = action.data.id;
+//       const noteToChange = state.find((n) => n.id === id);
+//       const changedNote = {
+//         ...noteToChange,
+//         important: !noteToChange.important,
+//       };
+//       return state.map((note) => (note.id !== id ? note : changedNote));
+//     }
+//     default:
+//       return state;
+//   }
+// };
 
 const store = createStore(noteReducer);
 
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
+// store.dispatch({
+//   type: "NEW_NOTE",
+//   data: {
+//     content: "the app state is in redux store",
+//     important: true,
+//     id: 1,
+//   },
+// });
 
-store.dispatch({
-  type: "NEW_NOTE",
-  data: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
+// store.dispatch({
+//   type: "NEW_NOTE",
+//   data: {
+//     content: "state changes are made with actions",
+//     important: false,
+//     id: 2,
+//   },
+// });
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+// const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
-const App = () => {
-  const createNote = (content) => {
-    return {
-      type: "NEW_NOTE",
-      data: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    };
-  };
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    //console.log(content, "content");
-    event.target.note.value = "";
-    // store.dispatch({
-    //   type: "NEW_NOTE",
-    //   data: {
-    //     content,
-    //     important: false,
-    //     id: generateId(),
-    //   },
-    // });
-
-    store.dispatch(createNote(content));
-  };
-
-  const toggleImportanceOf = (id) => {
-    return {
-      type: "TOGGLE_IMPORTANCE",
-      data: { id },
-    };
-  };
-
-  const toggleImportance = (id) => {
-    // store.dispatch({
-    //   type: "TOGGLE_IMPORTANCE",
-    //   data: { id },
-    // });
-
-    store.dispatch(toggleImportanceOf(id));
-  };
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const renderApp = () => root.render(<App />);
-renderApp();
-
-store.subscribe(renderApp);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
