e37c1860a6227d038d1d23080c7c9d0ed2cc18aa
nepsoul <nirumagar901@gmail.com>
2023-01-06 12:33:46 +0545

    chore: note created using action creator function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from "react-dom/client";
 import { createStore } from "redux";
 
 const noteReducer = (state = [], action) => {
-  if (action.type === "NEW_NOTE") {
-    //state.push(action.data);
-    //return state;
-    return [...state, action.data];
-  }
+  //   if (action.type === "NEW_NOTE") {
+  //     //state.push(action.data);
+  //     //return state;
+  //     return [...state, action.data];
+  //   }
 
-  return state;
+  //   return state;
+
+  switch (action.type) {
+    case "NEW_NOTE":
+      return state.concat(action.data);
+    case "TOGGLE_IMPORTANCE": {
+      const id = action.data.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    }
+    default:
+      return state;
+  }
 };
 
 const store = createStore(noteReducer);
@@ -35,26 +51,48 @@ store.dispatch({
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const App = () => {
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    console.log(content, "content");
-    event.target.note.value = "";
-    store.dispatch({
+  const createNote = (content) => {
+    return {
       type: "NEW_NOTE",
       data: {
         content,
         important: false,
         id: generateId(),
       },
-    });
+    };
+  };
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    //console.log(content, "content");
+    event.target.note.value = "";
+    // store.dispatch({
+    //   type: "NEW_NOTE",
+    //   data: {
+    //     content,
+    //     important: false,
+    //     id: generateId(),
+    //   },
+    // });
+
+    store.dispatch(createNote(content));
+  };
+
+  const toggleImportanceOf = (id) => {
+    return {
+      type: "TOGGLE_IMPORTANCE",
+      data: { id },
+    };
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: "TOGGLE_IMPORTANCE",
-      data: { id },
-    });
+    // store.dispatch({
+    //   type: "TOGGLE_IMPORTANCE",
+    //   data: { id },
+    // });
+
+    store.dispatch(toggleImportanceOf(id));
   };
 
   return (
@@ -63,7 +101,6 @@ const App = () => {
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {console.log(addNote, "addnote")}
       <ul>
         {store.getState().map((note) => (
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
