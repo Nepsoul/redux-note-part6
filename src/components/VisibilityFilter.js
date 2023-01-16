4a4b9b1a33d67f1d40d21a658ba57025e18f4153
nepsoul <nirumagar901@gmail.com>
2023-01-16 21:25:44 +0545

    chore: extracting visibility filter into its own component

---
+++ src/components/VisibilityFilter.js
@@ -0,0 +1,30 @@
+const VisibilityFilter = (props) => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
+  return (
+    <div>
+      all
+      <input
+        type="radio"
+        name="filter"
+        onChange={() => filterSelected("ALL")}
+      />
+      important
+      <input
+        type="radio"
+        name="filter"
+        onChange={() => filterSelected("IMPORTANT")}
+      />
+      nonimportant
+      <input
+        type="radio"
+        name="filter"
+        onChange={() => filterSelected("NONIMPORTANT")}
+      />
+    </div>
+  );
+};
+
+export default VisibilityFilter;
