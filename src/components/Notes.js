1cedbf8d370494a35424971619aed848f8279a8f
nepsoul <nirumagar901@gmail.com>
2023-01-16 22:05:56 +0545

    refactor: refactoring the state value code

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,15 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { toggleImportanceOf } from "../reducers/noteReducer";
 
-const Note = ({ note, handleClick }) => {
-  return (
-    <li onClick={handleClick}>
-      {note.content}
-      <strong> {note.important ? "important" : ""}</strong>
-    </li>
-  );
-};
-
 const Notes = () => {
   const dispatch = useDispatch();
   //const notes = useSelector((state) => state.notes);
@@ -30,11 +21,10 @@ const Notes = () => {
   return (
     <ul>
       {notes.map((note) => (
-        <Note
-          key={note.id}
-          note={note}
-          handleClick={() => dispatch(toggleImportance(note.id))}
-        />
+        <li key={note.id} onClick={() => toggleImportance(note.id)}>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
+        </li>
       ))}
     </ul>
   );
